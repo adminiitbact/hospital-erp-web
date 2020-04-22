07c87c712de3160c267f4e47ac920fed5d7c0074
Shubham Jain <shubhamjain0594@gmail.com>
2020-04-22 13:45:12 +0100

    Firebase as an environment variable.

--- prerun.js
+++ prerun.js
@@ -2,9 +2,14 @@ const fs = require('fs');
 
 api_endpoint = process.env.API_ENDPOINT;
 port = process.env.PORT;
+firebase = process.env.firebase;
 
 console.log(`API_ENDPOINT: ${api_endpoint}`);
 console.log(`PORT: ${port}`);
 
-fs.writeFileSync('public/js/globalconf.js', `API_ENDPOINT = "${api_endpoint}";`);
-fs.writeFileSync('.env', `VUE_APP_API_ENDPOINT=${api_endpoint}`);
+console.log(process.env.firebase);
+
+fs.writeFileSync('public/js/globalconf.js', `API_ENDPOINT = "${api_endpoint}";\n`);
+fs.appendFileSync('public/js/globalconf.js', `firebaseConfig = ${firebase}`);
+fs.writeFileSync('.env', `VUE_APP_API_ENDPOINT=${api_endpoint}\n`);
+fs.appendFileSync('.env', `VUE_APP_FIREBASE=${firebase}`);
--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const urljoin = require('url-join');
 
 const apiUrl = process.env.VUE_APP_API_ENDPOINT;
+const firebaseConfig = JSON.parse(process.env.VUE_APP_FIREBASE);
 
 const config = {
   apiUrl,
   userProfileFetchUrl: urljoin(apiUrl, '/hospital/user/profile'),
   fetchFacilityUrl: urljoin(apiUrl, 'facility/'),
+  firebaseConfig,
   severityMap: [
     'MILD',
     'MODERATE',
