3981f1119ad8a164357a923fe907e9afa822c2c4
Shubham Jain <shubhamjain0594@gmail.com>
2020-04-23 22:40:04 +0100

    Modified prerun to accept base64.

--- prerun.js
+++ prerun.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
 
-api_endpoint = process.env.API_ENDPOINT;
-port = process.env.PORT;
-firebase = process.env.firebase;
+const api_endpoint = process.env.API_ENDPOINT;
+const port = process.env.PORT;
+const base64Firebase = process.env.firebase;
 
 console.log(`API_ENDPOINT: ${api_endpoint}`);
 console.log(`PORT: ${port}`);
+console.log(`Base64 Firebase: ${base64Firebase}`);
 
-console.log(process.env.firebase);
+const firebaseConfig = Buffer.from(base64Firebase, 'base64').toString('utf8');
+
+console.log(firebaseConfig);
 
 fs.writeFileSync('public/js/globalconf.js', `API_ENDPOINT = "${api_endpoint}";\n`);
-fs.appendFileSync('public/js/globalconf.js', `firebaseConfig = ${firebase}`);
+fs.appendFileSync('public/js/globalconf.js', `firebaseConfig = ${firebaseConfig};`);
 fs.writeFileSync('.env', `VUE_APP_API_ENDPOINT=${api_endpoint}\n`);
-fs.appendFileSync('.env', `VUE_APP_FIREBASE=${firebase}`);
+fs.appendFileSync('.env', `VUE_APP_FIREBASE=${firebaseConfig}`);
--- public/js/globalconf.js
+++
@@ -1,2 +0,0 @@
-API_ENDPOINT = "http://api.staging.cov2.in/api/";
-firebaseConfig = {  "apiKey": "AIzaSyAf4j7k5TMTN86QoiNtonPc6zWodwGD5MM",  "authDomain": "hospital-erp-stag.firebaseapp.com",  "databaseURL": "https://hospital-erp-stag.firebaseio.com",  "projectId": "hospital-erp-stag",  "storageBucket": "hospital-erp-stag.appspot.com",  "messagingSenderId": "842907895704",  "appId": "1:842907895704:web:8445b521bf4819d68f8ded",  "measurementId": "G-CGHDBXB1Y0" }
