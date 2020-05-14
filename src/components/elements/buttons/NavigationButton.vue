0a6f1bd4c5f409f88621abb0f74ba9eb633a8d61
Shubham Jain <shubhamjain0594@gmail.com>
2020-05-14 11:09:52 +0100

    Basic layout and sidebar added.

---
+++ src/components/elements/buttons/NavigationButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <router-link
+    :to="to"
+    :class="{
+      'primary-navigation-button w-button': true,
+      'w--current': isActive
+    }">
+    <slot></slot>
+  </router-link>
+</template>
+<style scoped>
+.primary-navigation-button {
+  margin-right: 10px;
+  margin-bottom: 3px;
+  margin-left: 10px;
+  padding-right: 20px;
+  padding-left: 20px;
+  background-color: transparent;
+  color: #b9c3e4;
+  text-align: left;
+  text-decoration: none;
+}
+
+.primary-navigation-button.w--current {
+  border-radius: 10px;
+  background-color: #4f68ba;
+  color: #fff;
+}
+
+.primary-navigation-button:hover {
+  border-radius: 10px;
+  background-color: #4f68ba;
+  color: #fff;
+}
+</style>
+<script>
+import Vue from 'vue';
+
+const NavButProps = Vue.extend({
+  props: {
+    to: {
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+});
+
+export default class NavigationButton extends NavButProps {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
+import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import apolloClient from './hasuraConfig';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/css/custom.scss';
+
 Vue.config.productionTip = false;
 Vue.use(VueApollo);
+Vue.use(BootstrapVue);
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
