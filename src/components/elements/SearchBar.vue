b1a313b58b932cca68e3dff3d313602f82fe6192
Sapan Tanted <sapantanted99@gmail.com>
2020-05-16 01:53:17 +0530

    added elements: tag, searchbar, multiplebuttons; PatientOverview has their usage example

---
+++ src/components/elements/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input type="text" @change="$emit('change')" @keyup="$emit('keyup')"
+     :id="id" :name="name" :placeholder="placeholder" :maxlength="maxlength" class="input search w-input">
+  </div>
+</template>
+<style scoped>
+.input.search {
+  padding-left: 45px;
+  background-image: url('../../assets/images/Search-Blue.svg');
+  background-position: 0px 50%;
+  background-size: 35px;
+  background-repeat: no-repeat;
+}
+</style>
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+const SearchBarProps = Vue.extend({
+  props: {
+    placeholder: {
+      required: false,
+      default: 'Search',
+    },
+    maxlength: {
+      required: false,
+      default: '256',
+    },
+    id: {
+      required: true,
+    },
+    name: {
+      required: true,
+    },
+  },
+});
+
+@Component
+export default class SearchBar extends SearchBarProps {
+}
+</script>
---
+++ src/components/patients/PatientOverview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tab-pane-facility-profile w-tab-pane w--tab-active">
+    <multibuttons :buttons="mbuttons"></multibuttons>
+    <sbar :id="sbar" :name="sbar"></sbar>
+    <tag :color="'red'" :text="'Moderate'"></tag>
+    <tag :color="'green'" :text="'Moderate'"></tag>
+    <tag :color="'black'" :text="'Moderate'"></tag>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import MultipleButtons from '../elements/buttons/MultipleButtons.vue';
+import SearchBar from '../elements/SearchBar.vue';
+import Tag from '../elements/Tag.vue';
+
+
+const PatientOverviewProps = Vue.extend({
+  props: {
+  },
+});
+
+@Component({
+  components: {
+    multibuttons: MultipleButtons,
+    sbar: SearchBar,
+    tag: Tag,
+  },
+})
+export default class PatientOverview extends PatientOverviewProps {
+  mbuttons = [{ label: 'button1', click: () => console.log('button1') }, { label: 'butto2', click: () => console.log('button2') }, { label: 'button3', click: () => console.log('button3') }];
+}
+</script>
